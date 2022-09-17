998e843d1d8abdf0fba1581ab1dd2c0d6699b951
Caleb Li <49379586+ToothbrushB@users.noreply.github.com>
2022-09-17 15:06:54 -0500

    Added Test Belt Motor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestBeltMotor.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="Test Belt (John Cena) Motor")
+public class TestBeltMotor extends OpMode {
+
+    private DcMotor johnCenaMotor;
+    @Override
+    public void init() {
+        johnCenaMotor = hardwareMap.get(DcMotor.class, "John Cena Motor");
+        johnCenaMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        johnCenaMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    @Override
+    public void loop() {
+        float left_stick_y = gamepad1.left_stick_y;
+        johnCenaMotor.setPower(left_stick_y);
+    }
+}
