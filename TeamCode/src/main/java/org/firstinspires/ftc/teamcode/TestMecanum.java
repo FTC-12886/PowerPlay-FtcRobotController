cb14b7a25704fdf6c3e02269061ea2ac8b3beb72
Caleb Li <49379586+ToothbrushB@users.noreply.github.com>
2022-09-27 23:00:20 -0500

    Added motor directions and zpb settings; quadratic curve on the gamepad inputs for easier maneuvering; 0.5 speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMecanum.java
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+@TeleOp
 public class TestMecanum extends OpMode {
 
     private DcMotor rearRight;
@@ -11,31 +14,42 @@ public class TestMecanum extends OpMode {
     private DcMotor frontLeft;
     @Override
     public void init() {
-        rearRight = hardwareMap.get(DcMotor.class, "nameOfMotor");
-        rearLeft = hardwareMap.get(DcMotor.class, "nameOfMotor2");
-        frontRight = hardwareMap.get(DcMotor.class, "nameOfMotor3");
-        frontLeft = hardwareMap.get(DcMotor.class, "nameOfMotor4");
+        rearRight = hardwareMap.get(DcMotor.class, "rearRight");
+        rearLeft = hardwareMap.get(DcMotor.class, "rearLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
 
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        rearRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        rearLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rearRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rearLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @Override
     public void loop() {
-        float leftStickX = gamepad1.left_stick_x;
-        float leftStickY = gamepad1.left_stick_y;
-        float rightStickX = gamepad1.right_stick_x;
-        float rightStickY = gamepad1.right_stick_y;
+        double leftStickX = -gamepad1.left_stick_x*Math.abs(gamepad1.left_stick_x); // quadratic curve but flipped
+        double leftStickY = -gamepad1.left_stick_y*Math.abs(gamepad1.left_stick_y);
+        double rightStickX = -gamepad1.right_stick_x*Math.abs(gamepad1.right_stick_x);
+        double rightStickY = gamepad1.right_stick_y*Math.abs(gamepad1.right_stick_y);
         double rearRightPower = 0;
         double rearLeftPower = 0;
         double frontRightPower = 0;
         double frontLeftPower = 0;
 
         // mecanum code
+
+        // forwards/backwards
         rearRightPower += leftStickY;
         rearLeftPower += leftStickY;
         frontRightPower += leftStickY;
         frontLeftPower += leftStickY;
 
+        // sideways
         rearRightPower += leftStickX;
         rearLeftPower -= leftStickX;
         frontRightPower -= leftStickX;
@@ -48,9 +62,9 @@ public class TestMecanum extends OpMode {
         frontRightPower -= rightStickX;
         rearRightPower -= rightStickX;
 
-        rearRight.setPower(rearRightPower);
-        rearLeft.setPower(rearLeftPower);
-        frontRight.setPower(frontRightPower);
-        frontLeft.setPower(frontLeftPower);
+        rearRight.setPower(0.5*rearRightPower);
+        rearLeft.setPower(0.5*rearLeftPower);
+        frontRight.setPower(0.5*frontRightPower);
+        frontLeft.setPower(0.5*frontLeftPower);
     }
 }
