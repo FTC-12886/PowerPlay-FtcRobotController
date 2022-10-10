9aeffd36bf021ee67bba9e5824449dd610eb4f9e
Caleb Li <49379586+ToothbrushB@users.noreply.github.com>
2022-10-10 17:58:42 -0500

    Changed TestMecanum to work. Added GamepadTest and MotorDirectionTest.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMecanum.java
@@ -14,14 +14,14 @@ public class TestMecanum extends OpMode {
     private DcMotor frontLeft;
     @Override
     public void init() {
-        rearRight = hardwareMap.get(DcMotor.class, "rearRight");
-        rearLeft = hardwareMap.get(DcMotor.class, "rearLeft");
+        rearRight = hardwareMap.get(DcMotor.class, "backRight");
+        rearLeft = hardwareMap.get(DcMotor.class, "backLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
 
         frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        rearRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        rearRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         rearLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -32,10 +32,10 @@ public class TestMecanum extends OpMode {
 
     @Override
     public void loop() {
-        double leftStickX = -gamepad1.left_stick_x*Math.abs(gamepad1.left_stick_x); // quadratic curve but flipped
+        double leftStickX = gamepad1.left_stick_x*Math.abs(gamepad1.left_stick_x); // quadratic curve but flipped
         double leftStickY = -gamepad1.left_stick_y*Math.abs(gamepad1.left_stick_y);
-        double rightStickX = -gamepad1.right_stick_x*Math.abs(gamepad1.right_stick_x);
-        double rightStickY = gamepad1.right_stick_y*Math.abs(gamepad1.right_stick_y);
+        double rightStickX = gamepad1.right_stick_x*Math.abs(gamepad1.right_stick_x);
+        double rightStickY = -gamepad1.right_stick_y*Math.abs(gamepad1.right_stick_y);
         double rearRightPower = 0;
         double rearLeftPower = 0;
         double frontRightPower = 0;
@@ -50,10 +50,10 @@ public class TestMecanum extends OpMode {
         frontLeftPower += leftStickY;
 
         // sideways
-        rearRightPower += leftStickX;
-        rearLeftPower -= leftStickX;
-        frontRightPower -= leftStickX;
-        frontLeftPower += leftStickX;
+        rearRightPower -= leftStickX;
+        rearLeftPower += leftStickX;
+        frontRightPower += leftStickX;
+        frontLeftPower -= leftStickX;
 
         // turning
         frontLeftPower += rightStickX;
@@ -62,9 +62,14 @@ public class TestMecanum extends OpMode {
         frontRightPower -= rightStickX;
         rearRightPower -= rightStickX;
 
-        rearRight.setPower(0.5*rearRightPower);
-        rearLeft.setPower(0.5*rearLeftPower);
-        frontRight.setPower(0.5*frontRightPower);
-        frontLeft.setPower(0.5*frontLeftPower);
+        telemetry.addData("FL", frontLeftPower);
+        telemetry.addData("FR", frontRightPower);
+        telemetry.addData("RL", rearLeftPower);
+        telemetry.addData("RR", rearRightPower);
+
+        rearRight.setPower((gamepad1.left_bumper ? 1 : 0.5)*rearRightPower);
+        rearLeft.setPower((gamepad1.left_bumper ? 1 : 0.5)*rearLeftPower);
+        frontRight.setPower((gamepad1.left_bumper ? 1 : 0.5)*frontRightPower);
+        frontLeft.setPower((gamepad1.left_bumper ? 1 : 0.5)*frontLeftPower);
     }
 }
