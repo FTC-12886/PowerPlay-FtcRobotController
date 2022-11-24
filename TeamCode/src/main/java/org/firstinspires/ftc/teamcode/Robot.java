18f7f0023041ebff82920a664e15c0e95bbf3a9e
Caleb Li <49379586+ToothbrushB@users.noreply.github.com>
2022-11-24 11:14:41 -0600

    started auto, robotspecifications class, changed testmecanum to a "port view" like thing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class Robot {
+    private DcMotorEx rearRight;
+    private DcMotorEx rearLeft;
+    private DcMotorEx frontRight;
+    private DcMotorEx frontLeft;
+    private BNO055IMU imu;
+}
